f042302b49a0ef314b5432e0d83a8d76fcb903a0
LuisFer666 <>
2020-12-01 10:34:55 -0600

    - GUI Base actualización 1

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -9,7 +9,6 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class Main extends javax.swing.JFrame {
     
     public Main() {
-            this.setLocationRelativeTo(null);
             initComponents();
             try {
                 UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
@@ -50,11 +49,6 @@ public class Main extends javax.swing.JFrame {
         setPreferredSize(new java.awt.Dimension(325, 504));
         setResizable(false);
         setSize(new java.awt.Dimension(325, 504));
-        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
-            public void mouseDragged(java.awt.event.MouseEvent evt) {
-                formMouseDragged(evt);
-            }
-        });
 
         jPanel1.setBackground(new java.awt.Color(238, 238, 238));
 
@@ -64,11 +58,6 @@ public class Main extends javax.swing.JFrame {
         jTextField1.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
         jTextField1.setText("8x9e8r7v9s9e+f-c");
         jTextField1.setBorder(null);
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
 
         jTextField2.setEditable(false);
         jTextField2.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 40)); // NOI18N
@@ -109,11 +98,6 @@ public class Main extends javax.swing.JFrame {
         jButton2.setMaximumSize(new java.awt.Dimension(50, 29));
         jButton2.setMinimumSize(new java.awt.Dimension(50, 29));
         jButton2.setPreferredSize(new java.awt.Dimension(50, 29));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
 
         jButton3.setText("M+");
         jButton3.setBorder(null);
@@ -121,11 +105,6 @@ public class Main extends javax.swing.JFrame {
         jButton3.setMaximumSize(new java.awt.Dimension(50, 29));
         jButton3.setMinimumSize(new java.awt.Dimension(50, 29));
         jButton3.setPreferredSize(new java.awt.Dimension(50, 29));
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
 
         jButton4.setText("M-");
         jButton4.setBorder(null);
@@ -133,11 +112,6 @@ public class Main extends javax.swing.JFrame {
         jButton4.setMaximumSize(new java.awt.Dimension(50, 29));
         jButton4.setMinimumSize(new java.awt.Dimension(50, 29));
         jButton4.setPreferredSize(new java.awt.Dimension(50, 29));
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
 
         jButton5.setText("MS");
         jButton5.setBorder(null);
@@ -145,11 +119,6 @@ public class Main extends javax.swing.JFrame {
         jButton5.setMaximumSize(new java.awt.Dimension(50, 29));
         jButton5.setMinimumSize(new java.awt.Dimension(50, 29));
         jButton5.setPreferredSize(new java.awt.Dimension(50, 29));
-        jButton5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
-            }
-        });
 
         jButton6.setText("M");
         jButton6.setBorder(null);
@@ -224,30 +193,6 @@ public class Main extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
-
-    }//GEN-LAST:event_formMouseDragged
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton4ActionPerformed
-
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton5ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
